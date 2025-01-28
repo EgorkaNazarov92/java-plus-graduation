5407c96b17debd0935fa496edec729f30b1fc672
Egor Nazarov <nazarovegorka@yandex.ru>
2025-01-28 18:37:47 +0300

    fix

--- core/main-service/src/main/java/ewm/MainApplication.java
+++ core/main-service/src/main/java/ewm/MainApplication.java
@@ -2,10 +2,12 @@ package ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(clients = {StatsClient.class})
+@EnableDiscoveryClient
 public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
